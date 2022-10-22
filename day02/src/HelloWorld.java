0584870a1a1ca8964840e61b493ae61dd622c33d
GyudeokJO <77923806+JoygyuD@users.noreply.github.com>
2022-10-22 16:29:38 +0900

    Delete day02 directory

--- day02/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hello World!"); // sysout 입력후 ctrl+Space  System.out.println 자동완성.
-	}
-}
