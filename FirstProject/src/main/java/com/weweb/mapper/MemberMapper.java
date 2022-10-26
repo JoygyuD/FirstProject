7db70a290a84d079f55a89e81499ef5dc99f9101
GyudeokJO <77923806+JoygyuD@users.noreply.github.com>
2022-10-26 15:43:17 +0900

    Delete FirstProject directory

--- FirstProject/src/main/java/com/weweb/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.weweb.mapper;
-
-import com.weweb.command.MemberVO;
-
-public interface MemberMapper {
-	
-	public MemberVO login(MemberVO vo); 	// 로그인
-	public int signUp(MemberVO vo);	// 회원가입
-}
